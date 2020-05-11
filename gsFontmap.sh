46a9900e9b15d901d4a4c0f498372ef16622c01f
beginner3456789 <commander1oldsmobile@yahoo.com>
2020-05-11 08:30:14 -0700

    Update gsFontmap.sh

--- gsFontmap.sh
+++ gsFontmap.sh
@@ -4,7 +4,7 @@
 # change this GS_FONTPATH to search your directories and add to Fontmap
 # the optional GS_OPTIONS might be needed and can be changed as desired
 
-GS_OPTIONS="-dNONATIVEFONTMAP -dNOPLATFONTS" \
+#GS_OPTIONS="-dNONATIVEFONTMAP -dNOPLATFONTS" \
 GS_FONTPATH="/usr/share/ghostscript/fonts:/usr/local/share/fonts" \
 gs -dBATCH -dNODISPLAY -dNOFONTMAP -c '/none findfont loadallfonts' | \
 grep Loading | \
