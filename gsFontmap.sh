8fe51bace2be9fe424844987d664341620b255a8
beginner3456789 <commander1oldsmobile@yahoo.com>
2020-05-31 17:10:52 -0700

    Update gsFontmap.sh

--- gsFontmap.sh
+++ gsFontmap.sh
@@ -9,7 +9,7 @@ GS_FONTPATH="/usr/share/ghostscript/fonts:/usr/local/share/fonts" \
 gs -dBATCH -dNODISPLAY -dNOFONTMAP -c '/none findfont loadallfonts' | \
 grep Loading | \
 sed -e 's/\.\.\./   /' | \
-awk '{print "/"$2 " (" $5 ");"}' | \
+awk '{print "/"$2 " (" $5 ") ;"}' | \
 sort > /usr/share/ghostscript/"$(gs --version)"/Resource/Font/Fontmap
 
 exit 0
